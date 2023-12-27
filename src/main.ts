b77e00f95a02f6a5fba721f67a521fb6bc6e75c1
Lam Tran <quanglam0509@technixo.com>
2023-12-27 11:36:26 +0700

    add docker

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+require('dotenv').config();
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
