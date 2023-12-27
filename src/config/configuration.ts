879116ba03f363901bd7a7d3e1c1d8738c0f13fc
Lam Tran <quanglam0509@technixo.com>
2023-12-27 11:36:26 +0700

    add docker

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,7 +3,7 @@ require('dotenv').config();
 export const Configuration = () => ({
   signedMessage: process.env.signedMessage,
   security: {
-    jwtSecret: process.env.JWT_SECRET,
-    accessTokenExpiry: process.env.ACCESS_TOKEN_EXPIRY,
+    jwtSecret: process.env.JWT_SECRET || 'nosecret',
+    accessTokenExpiry: process.env.ACCESS_TOKEN_EXPIRY || '1h',
   },
 });
