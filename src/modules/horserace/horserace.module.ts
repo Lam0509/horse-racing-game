3f9d7e37ac50b1c517811f26d9404f9b1533aa5b
Lam Tran <quanglam0509@technixo.com>
2024-01-05 15:59:06 +0700

    add get room api

--- src/modules/horserace/horserace.module.ts
+++ src/modules/horserace/horserace.module.ts
@@ -5,10 +5,12 @@ import { HorseRaceGateway } from './horserace.gateway';
 import { HorseRaceService } from './horserace.service';
 import { CacheModule } from '../../providers/cache/cache.module';
 import { HistoryModule } from '../history/history.module';
+import { HorseraceController } from './horserace.controller';
 
 @Module({
   imports: [UserModule, HistoryModule, SocketModule, CacheModule],
   providers: [HorseRaceGateway, HorseRaceService],
   exports: [HorseRaceService],
+  controllers: [HorseraceController],
 })
 export class HorseRaceModule {}
